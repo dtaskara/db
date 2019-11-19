3ab84f13841dbb97ca6466bf93c5db33e2f84897
Aknur <aknur.zhagypar@mail.ru>
2019-11-19 22:11:16 +0600

    db query

---
+++ api/hotels/getAll.php
@@ -0,0 +1,24 @@
+<?php
+
+	include '../../database/connection.php';
+	$query=sqlsrv_query($conn,"SELECT * FROM Hotel;");
+	$res=array();
+
+echo "string";
+echo sqlsrv_num_rows($query);
+echo "string";
+	if(sqlsrv_num_rows($query)>0){
+		while ( $row=sqlsrv_fetch_object( $query )) {
+			$res[]=array(
+				"id"=>$row->H_id,
+				"name"=>$row->H_name,
+				"contact"=>$row->H_contact,
+				"city_id"=>$row->City_id,
+				"price"=>$row->H_price,
+				"stars"=>$row->H_stars
+			)
+			;	
+		}
+	}
+	echo json_encode($res);
+?>
